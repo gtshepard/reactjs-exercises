f20f5ea9caea4b534f839ced7b644979c19952c7
Garrison Shepard <shepard.garrisont@gmail.com>
2019-03-05 13:50:30 -0500

    feat: nav bar

---
+++ my-first-app/src/greet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Greet = () => {
+	return <h2> Bonjour</h2>
+}
+
+export default Greet;
---
+++ my-first-app/src/header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+     <header id= "header">
+      <div class="container" >
+        <div class="logo">
+          <a href="#"> Logo </a>
+        </div>
+        <div class="nav">
+          <nav>
+            <ul>
+              <li><a href="">Home </a> </li>
+              <li><a href="">About Us </a> </li>
+              <li><a href="">Contact us </a> </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+     </header>
+	);
+}
+export default Header;
